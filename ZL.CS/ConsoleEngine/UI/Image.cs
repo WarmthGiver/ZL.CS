5c2d7a9b7856d491b115eb7e623dc4e4f8a97b61
ZION <warmth.giver@gmail.com>
2024-12-12 00:19:03 +0900

    Rebuild project
    프로젝트 재구성

--- ZL.CS/ConsoleEngine/UI/Image.cs
+++
@@ -1,9 +0,0 @@
-﻿using ZL.CS.Graphics;
-
-namespace ZL.CS.ConsoleEngine.UI
-{
-    public sealed class Image : UI<Background>
-    {
-        public Image(ConsoleObject sceneObject, Background? background = null) : base(sceneObject, background) { }
-    }
-}
--- ZL.CS/ConsoleEngine/UI/Text.cs
+++
@@ -1,9 +0,0 @@
-﻿using ZL.CS.Graphics;
-
-namespace ZL.CS.ConsoleEngine.UI
-{
-    public sealed class Text : UI<Foreground>
-    {
-        public Text(ConsoleObject sceneObject, Foreground? foreground) : base(sceneObject, foreground) { }
-    }
-}
